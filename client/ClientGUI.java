a2904f699da96daf1bca14bdd34171c4dcc060e5
RiyaNarang24 <itsriya391@gmail.com>
2025-11-14 21:49:53 +0530

    Update ClientGUI.java

--- client/ClientGUI.java
+++ client/ClientGUI.java
@@ -25,7 +25,8 @@ public class ClientGUI {
         canvas = new CanvasPanel(600, 450);
         canvas.setDrawListener(cmd -> {
             // prefix with username when sending
-            client.send("DRAW:" + username + ":" + cmd.substring(5)); // remove leading "DRAW:"
+            client.send("DRAW:" + username + ":" + cmd.split(":", 2)[1]);
+// remove leading "DRAW:"
         });
 
         JPanel toolbar = new JPanel();
